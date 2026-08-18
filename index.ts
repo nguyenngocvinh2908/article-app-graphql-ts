6723511c53227263d044fbbe01bba10cbb302a04
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-08-18 10:53:12 +0700

    [#2] Connect Database

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import express, { Express, Request, Response } from 'express'
+// Env
+import dotenv from 'dotenv'
+dotenv.config()
+
+// Connect Database
+import * as database from './config/database'
+import Article from './models/article'
+database.connectDatabase()
 
 const app: Express = express()
-const port: number = 3000
+const port: number | string = process.env.PORT || 3000;
 
 // Rest API
-app.get('/articles', (req: Request, res: Response) => {
+app.get('/articles', async (req: Request, res: Response) => {
+  const articles = await Article.find({ deleted: false })
   res.json({
-    articles: []
+    articles: articles
   })
 })
 
