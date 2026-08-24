197ce43a8584e706b28c10776e7c48939ac57d43
Vinh Ga <nguyenngocvinh770@gmai.com>
2026-08-24 14:21:51 +0700

    [#7] CRUD Category

--- resolvers.ts
+++ resolvers.ts
@@ -1,39 +1,48 @@
 import Article from "./models/article"
-interface GetArticlesArgs {
-  id: string,
-}
-interface ArticleInput {
-  title: string;
-  avatar?: string;
-  description?: string;
-}
-interface CreateArticleArgs {
-  article: ArticleInput;
-}
-
-interface UpdateArticleArgs {
-  id: string,
-  article: ArticleInput 
-}
-
-interface DeleteArticleArgs {
+import Category from "./models/category"
+interface ByIdArgs {
   id: string
 }
+interface ArticleInput {
+  title: string
+  avatar?: string
+  description?: string
+}
+interface CategoryInput {
+  title: string,
+  avatar?: string
+}
+
+interface CreateCategoryArgs {
+  category: CategoryInput
+}
+interface CreateArticleArgs {
+  article: ArticleInput
+}
+
+type UpdateArticleArgs = ByIdArgs & CreateArticleArgs
+type UpdateCategoryArgs = ByIdArgs & CreateCategoryArgs
+
 
 export const resolvers = { 
   Query: {
-
-    hello: () => {
-      return "Hello World"
-    },
     getListArticle: async () => {
-      const article = await Article.find({ deleted: false })
-      return article
+      const articles = await Article.find({ deleted: false })
+      return articles
     },
-    getArticle: async (_: unknown, args: GetArticlesArgs) => {
+    getArticle: async (_: unknown, args: ByIdArgs) => {
       const { id } = args
       const article = await Article.findOne({ _id: id, deleted: false })
       return article
+    },
+    getListCategory: async () => {
+      const categories = await Category.find({ deleted: false })
+      return categories
+    },
+    getCategory: async (_: unknown, args: ByIdArgs) => {
+      const { id } = args
+      const category = await Category.findOne({ _id: id, deleted: false })
+      return category
     }
   },
   Mutation: {
@@ -45,12 +54,28 @@ export const resolvers = {
     },
     updateArticle: async (_: unknown, args: UpdateArticleArgs) => {
       const { id, article } = args
-      const updatedArticle = await Article.findByIdAndUpdate( id,{ $set: article },{ new: true } );
+      const updatedArticle = await Article.findByIdAndUpdate( id,{ $set: article },{ new: true } )
       return updatedArticle
     },
-    deleteArticle: async (_: unknown , args: DeleteArticleArgs) => {
+    deleteArticle: async (_: unknown , args: ByIdArgs) => {
       const { id } = args
       await Article.updateOne({ _id: id}, { deleted: true, deletedAt: Date.now() })
+      return "Success"
+    },
+    createCategory: async (_: unknown, args: CreateCategoryArgs) => {
+      const { category } = args
+      const record = new Category(category)
+      await record.save()
+      return record
+    },
+    updateCategory: async (_: unknown, args: UpdateCategoryArgs) => {
+      const { id, category } = args
+      const updateCategory = await Category.findByIdAndUpdate( id, { $set: category }, { new: true })
+      return updateCategory
+    },
+    deleteCategory: async (_: unknown, args: ByIdArgs) => {
+      const { id } = args
+      await Category.updateOne({ _id: id }, { deleted: true, deletedAt: Date.now() })
       return "Success"
     }
   }
--- typedefs.ts
+++ typedefs.ts
@@ -8,21 +8,36 @@ export const typeDefs = gql`
     description: String
   }
 
+  type Category {
+    id: ID,
+    title: String,
+    avatar: String,
+  }
+
   type Query {
-    hello: String,
     getListArticle: [Article],
-    getArticle(id: ID): Article
+    getArticle(id: ID): Article,
+    getListCategory: [Category],
+    getCategory(id: ID): Category
   }
 
   input ArticleInput {
     title: String,
     avatar: String,
     description: String
-  } 
+  }
+
+  input CategoryInput {
+    title: String,
+    avatar: String,
+  }
 
   type Mutation {
     createArticle(article: ArticleInput): Article,
     updateArticle(id: ID, article: ArticleInput): Article,
-    deleteArticle(id: ID): String
+    deleteArticle(id: ID): String,
+    createCategory(category: CategoryInput): Category,
+    updateCategory(id: ID, category: CategoryInput): Category,
+    deleteCategory(id: ID): String
   }
 `
